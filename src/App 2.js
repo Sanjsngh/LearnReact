78d877747b866ebd799ccf3e495a66b2e6ce83a2
sanjsngh <sanjivanisngh@gmail.com>
2023-03-30 18:54:59 +0530

    Counter App

---
+++ src/App 2.js
@@ -0,0 +1,34 @@
+function App() {
+    const inputType = "number";
+    const minValue = 5;
+    const maxValue = 10;
+    // let msg = "By there!"
+
+    // if(Math.random() > 0.5) {
+    //     msg = "Hello there!";
+    // }
+
+    // const date = new Date();
+    // const time = date.toLocaleTimeString();
+
+    // curly braces reference a JS variable 
+    // curly braces to show strings or numbers
+    // (unexpected behaviour with boolean, undefined, null, array and objects)
+    // props don't have to be defined as variables
+    // if it's a string, wrap with double quotes
+    // for numbers, wrap with curly braces
+    // Array: wrap with curly brackets
+    // Object: wrap with curly brackets
+
+    return <input spellCheck />
+    // <textarea autoFocus={false} />
+            // <input 
+            //     style={{border:'3px solid red'}}
+            //     type="number"
+            //     min={6}
+            //     list={[1,2,3]}
+            //     // max={maxValue}
+            // />
+}
+
+export default App;
